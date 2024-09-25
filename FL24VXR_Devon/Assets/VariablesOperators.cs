f711e569aecbf1fb690cbe93324f301c56066b96
ItBeCatPuns <therealsgreninja+github@gmail.com>
2024-09-25 09:08:02 -0600

    Added VariableOperators

---
+++ FL24VXR_Devon/Assets/VariablesOperators.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesOperators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int turtleOwned = 79;
+        float turtleValue = 19.96f;
+        string playerName = "Devon Shields";
+        bool isHuman = true;
+    }
+}
